27014097004fd331d4534ece24125a7fa7f62e95
Chido Ukaigwe <chidodesigns@gmail.com>
2020-09-25 19:16:06 +0100

    we have a reusable to textfield - validation errors to show

---
+++ resources/js/components/InputField.vue
@@ -0,0 +1,44 @@
+<template>
+     <div class="form-group mb-3">
+            <label :for="title">{{label}}</label>
+            <input type="text" :id="title" class="form-control" :placeholder="placeholder" v-model="value" @input="updateField()">
+
+            <p class="small text-danger" v-text="errorMessage(title)"></p>
+     </div>
+</template>
+
+<script>
+export default {
+    name: "InputField",
+
+    props: [
+        'title',
+        'label',
+        'placeholder',
+        'errors'
+    ],
+
+    data: function () {
+        return {
+            value: '',
+        }
+    },
+
+    methods: {
+        updateField: function () {
+            this.$emit('update:field', this.value)
+        },
+
+        errorMessage: function (field) {
+            if (this.errors && this.errors[field] && this.errors[field].length > 0){
+                return this.errors[field][0];
+            }
+        }
+
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/TextareaField.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <label :for="body" class="mt-3">{{label}}</label>
+        <div class="form-group" id="editor">
+            <textarea class="form-control" :id="body"  v-model="value" @textarea="updateTextarea()"></textarea>
+        </div>
+        <div>
+            <p class="small text-danger" v-text="errorMessage(body)">Error</p>
+        </div>
+
+    </div>
+
+</template>
+
+<script>
+export default {
+
+    name: "TextareaField",
+
+    props: [
+        'body',
+        'label',
+        'placeholder',
+        'errors'
+    ],
+
+    data: function () {
+        return {
+            value: '',
+        }
+    },
+
+    methods: {
+
+        updateTextarea: function () {
+            this.emit('update:textarea', this.value)
+        },
+
+        errorMessage:  function (field) {
+            if (this.errors && this.errors[field] && this.errors[field].length > 0){
+                return this.errors[field][0];
+            }
+        }
+
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
+
